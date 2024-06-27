a4a88a4bb692f260846837a99b5fd8174c961bda
Ariasreymer10 <ariasreymer10@gmail.com>
2024-06-27 17:16:18 -0500

    Avance de Proyect Funciones

--- Proyectos Python Aplicativos/001_capital_trabajo.py
+++ Proyectos Python Aplicativos/001_capital_trabajo.py
@@ -1,1 +1,12 @@
-#analisis de Infi
+#Análisis de los La Rentabilidad de los Productos
+
+tipos_productos = ["Laptops","Tablets","Smartphones","Impresoras","Monitores"]
+
+codigos_prodcuto = ["P001","P002","P003","P004","P005"]
+
+precios_producto = [1200,500,300,200,150]
+
+costos_producto = [800,300,200,150,100]
+
+cantidades_vendidas = [100,200,300,400,500]
+
